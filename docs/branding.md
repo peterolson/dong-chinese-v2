563cbaae8ffb2b06e5b0d9b21ea2c2bf92a91d06
Peter Olson <peter.e.c.olson+github@gmail.com>
2026-02-13 22:52:09 -0600

    basic branding

---
+++ docs/branding.md
@@ -0,0 +1,23 @@
+# Dong Chinese Branding
+
+Logos and palette designed by [Ivan Raszl](https://github.com/iraszl)
+
+![Dong Chinese Branding image](img/branding.png)
+
+## Colors
+
+- Primary color (crimson): #ac0045
+- Secondary color (blue): #0066ab
+- Tertiary color (turquoise): #009e7f
+- Light Gray: #9b9b9b
+- Dark Gray: #333333
+
+## Icons
+
+<img src="img/koi-icon-positive.png" alt="Koi Icon Positive" width="200"/>
+<img src="img/koi-icon-negative.png" alt="Koi Icon Negative" width="200"/>
+
+## Logos
+
+<img src="img/koi-logo-positive.png" alt="Koi Logo Positive" width="400"/>
+<img src="img/koi-logo-negative.png" alt="Koi Logo Negative" width="400"/>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
+	import favicon96 from '$lib/assets/favicon-96.png';
+	import favicon64 from '$lib/assets/favicon-64.png';
+	import favicon32 from '$lib/assets/favicon-32.png';
+	import favicon16 from '$lib/assets/favicon-16.png';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
 	import { resolve } from '$app/paths';
 	import AuthStatus from '$lib/components/auth-status.svelte';
 	import type { LayoutData } from './$types';
+	import './global.css';
 
 	let { data, children }: { data: LayoutData; children: import('svelte').Snippet } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>懂中文 Dong Chinese</title>
+	<link rel="shortcut icon" href={favicon} />
+	<link rel="icon" sizes="16x16 32x32 64x64" href={favicon} />
+	<link rel="icon" type="image/png" sizes="96x96" href={favicon96} />
+	<link rel="icon" type="image/png" sizes="64x64" href={favicon64} />
+	<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+	<link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
 </svelte:head>
 
 <header class="site-header">
-	<a href={resolve('/')} class="site-name">Dong Chinese</a>
+	<a href={resolve('/')} class="site-name">懂中文 Dong Chinese</a>
 	<AuthStatus user={data.user} />
 </header>
 
